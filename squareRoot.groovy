3aca8766dae48e95a4d1336b60eb40a721797202
Ranjan <AR035595@Cerner.net>
2022-09-27 22:25:38 +0530

    cerner_2tothe5th_2022 Groovy code to find square root

---
+++ squareRoot.groovy
@@ -0,0 +1,10 @@
+/* cerner_2tothe5th_2022 */
+/* The following code determines the square root of a number */
+
+class Example { 
+   static void main(String[] args) { 
+      double value = 11.635;
+	  
+      System.out.printf("sqrt(%.3f) is %.3f%n", value, Math.sqrt(value)); 
+   } 
+}
