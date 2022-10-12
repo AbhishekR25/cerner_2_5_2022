7ef50a71bf08ceb8a902bb378d72458c477b823e
Ranjan <AR035595@Cerner.net>
2022-10-12 10:10:29 +0530

    cerner_2tothe5th_2022 Rust program to print Fibonacci sequence

---
+++ FibonnaciSequence.rs
@@ -0,0 +1,15 @@
+// cerner_2tothe5th_2022
+const ITERS: usize = 20;
+
+fn print_fib(n: usize) {
+    let mut x = (1, 1);
+    for i in 0..n {
+        println!("{}: {}", i, x.0);
+        x = (x.1, x.0 + x.1)
+    }
+}
+
+fn main() {
+    println!("# print_fib");
+    print_fib(ITERS);
+}
